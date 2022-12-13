ee119d2ea91b6452046c65e69e73610021ddc361
salmonte91 <stevenalmonte4@gmail.com>
2022-12-12 21:28:01 -0500

    created function to create images

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const mainMenu = document.querySelector('#main-menu');
 const startBtn = document.querySelector('#startBtn');
+const grid = document.querySelector('.grid');
+
+const blockTiles = ['./assets/Colored-Tiles/Blue-Tiles.png', './assets/Colored-Tiles/Brown-Tiles.png', './assets/Colored-Tiles/Gray-Tiles.png', './assets/Colored-Tiles/Green-Tiles.png', './assets/Colored-Tiles/Orange-Tiles.png', './assets/Colored-Tiles/Purple-Breakout-Tiles.png', './assets/Colored-Tiles/Red-Tiles.png', './assets/Colored-Tiles/Yellow-Tiles.png']
 
 function startGame(){
     mainMenu.remove();
@@ -7,3 +10,11 @@ function startGame(){
 
 startBtn.addEventListener('click', startGame)
 
+
+function createBlocks(){
+    let block = document.createElement('img')
+        block.src = './assets/Colored-Tiles/Blue-Tiles.png'
+        grid.appendChild(block)
+}
+
+createBlocks()
