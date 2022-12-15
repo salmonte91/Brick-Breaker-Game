23ea827ab9ff3102465a1eb96386796d06dd821d
salmonte91 <stevenalmonte4@gmail.com>
2022-12-14 19:46:44 -0500

    created blocks and added paddle

--- main.js
+++ main.js
@@ -2,19 +2,38 @@ const mainMenu = document.querySelector('#main-menu');
 const startBtn = document.querySelector('#startBtn');
 const grid = document.querySelector('.grid');
 
-const blockTiles = ['./assets/Colored-Tiles/Blue-Tiles.png', './assets/Colored-Tiles/Brown-Tiles.png', './assets/Colored-Tiles/Gray-Tiles.png', './assets/Colored-Tiles/Green-Tiles.png', './assets/Colored-Tiles/Orange-Tiles.png', './assets/Colored-Tiles/Purple-Breakout-Tiles.png', './assets/Colored-Tiles/Red-Tiles.png', './assets/Colored-Tiles/Yellow-Tiles.png']
+const blockTiles = ['./assets/Colored-Tiles/Blue-Tiles.png', './assets/Colored-Tiles/Green-Tiles.png', './assets/Colored-Tiles/Orange-Tiles.png', './assets/Colored-Tiles/Purple-Breakout-Tiles.png', './assets/Colored-Tiles/Red-Tiles.png', './assets/Colored-Tiles/Yellow-Tiles.png']
+
+const animatePaddle = ['./assets/Paddle-images/Paddle-Animate1.png', './assets/Paddle-images/Paddle-Animate2.png', './assets/Paddle-images/Paddle-Animate3.png']
 
 function startGame(){
     mainMenu.remove();
+    for(let i = 0; i < blockTiles.length; i++) //creates multiple blocks of same color
+{
+  
+  for(let j = 0; j < 19; j++)
+  {
+    createBlocks(blockTiles[i])
+  }
+}
+    createPaddle('./assets/Paddle-images/Paddle-Animate1.png')
 }
 
 startBtn.addEventListener('click', startGame)
 
 
-function createBlocks(){
+function createBlocks(url){
     let block = document.createElement('img')
-        block.src = './assets/Colored-Tiles/Blue-Tiles.png'
+        block.src = url
         grid.appendChild(block)
+        return block
 }
 
-createBlocks()
+function createPaddle(url){
+    let paddle = document.createElement('img')
+        paddle.src = url
+        paddle.style.marginTop = '750px'
+        paddle.style.width = '100px'
+        grid.appendChild(paddle)
+        return paddle
+}
