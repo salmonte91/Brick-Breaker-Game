f050933d6a86f200896cf0a88c7b3d8cc3fb3964
salmonte91 <stevenalmonte4@gmail.com>
2022-12-19 20:01:26 -0500

    added paddle movement

--- main.js
+++ main.js
@@ -2,21 +2,22 @@ const mainMenu = document.querySelector('#main-menu');
 const startBtn = document.querySelector('#startBtn');
 const grid = document.querySelector('.grid');
 
+grid.style.visibility = "hidden";
+
+
 const blockTiles = ['./assets/Colored-Tiles/Blue-Tiles.png', './assets/Colored-Tiles/Green-Tiles.png', './assets/Colored-Tiles/Orange-Tiles.png', './assets/Colored-Tiles/Purple-Breakout-Tiles.png', './assets/Colored-Tiles/Red-Tiles.png', './assets/Colored-Tiles/Yellow-Tiles.png']
 
 const animatePaddle = ['./assets/Paddle-images/Paddle-Animate1.png', './assets/Paddle-images/Paddle-Animate2.png', './assets/Paddle-images/Paddle-Animate3.png']
 
 function startGame(){
     mainMenu.remove();
-    for(let i = 0; i < blockTiles.length; i++) //creates multiple blocks of same color
-{
-  
-  for(let j = 0; j < 19; j++)
-  {
-    createBlocks(blockTiles[i])
-  }
-}
-    createPaddle('./assets/Paddle-images/Paddle-Animate1.png')
+    grid.style.visibility = "visible";
+    for(let i = 0; i < blockTiles.length; i++){ //creates multiple blocks of same color
+        for(let j = 0; j < 19; j++){
+            createBlocks(blockTiles[i])
+        }
+    }
+    createPaddle(0)
 }
 
 startBtn.addEventListener('click', startGame)
@@ -29,11 +30,49 @@ function createBlocks(url){
         return block
 }
 
-function createPaddle(url){
-    let paddle = document.createElement('img')
-        paddle.src = url
-        paddle.style.marginTop = '750px'
-        paddle.style.width = '100px'
-        grid.appendChild(paddle)
-        return paddle
+function createPaddle(xAxis) {
+    var paddle = document.createElement('img')
+    paddle.src = './assets/Paddle-images/Paddle-Animate1.png'
+    paddle.style.marginTop = '650px'
+    paddle.style.width = '100px'
+    paddle.style.left = xAxis + 'px'
+    grid.appendChild(paddle)
+
+    let direction
+
+
+    document.addEventListener('keydown', function(move){
+        if(move.key === 'ArrowLeft'){
+        direction = 'left'
+           if(xAxis === -720){
+                direction = null
+            }
+        }
+        else if(move.key === 'ArrowRight'){
+        direction = 'right'
+            if(xAxis === 720){
+                direction = null
+            }
+        }
+          xAxis =  movePaddle(direction,xAxis,paddle)
+
+    })
+    document.addEventListener('keyup', ()=> {
+        direction = null
+    })
+
+}
+
+function movePaddle(direction, x, paddle){
+
+        if(direction === 'left'){
+            x-=20
+        }
+        else if(direction === 'right'){
+            x+=20
+        }
+        
+        paddle.style.left = x + 'px'
+
+        return x;
 }
